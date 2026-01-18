3c1beda2342d7274e8e33e6b0ce2e57cbe8d3842
sangramsekhar81 <sangramsekhar83@gmail.com>
2026-01-18 17:52:48 +0530

    update controller

--- src/main/java/com/spring/security/controller/AdminController.java
+++ src/main/java/com/spring/security/controller/AdminController.java
@@ -1,14 +1,22 @@
 package com.spring.security.controller;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.security.model.Doctor;
+import com.spring.security.model.User;
 import com.spring.security.repo.DoctorRepository;
+import com.spring.security.repo.UserRepository;
 
 @RestController
 public class AdminController {
@@ -16,11 +24,25 @@ public class AdminController {
 	@Autowired
 	private DoctorRepository doctorRepo;
 	
+	@Autowired
+	private UserRepository userRepo;
+	
 	
 	@PostMapping("/add-doctor")
 	public Doctor addDocktor(@RequestBody Doctor doctor) {
 		return doctorRepo.save(doctor);
 	}
+	
+	@GetMapping("/findAll")
+	public List<Doctor> findAll(){
+		return doctorRepo.findAll();
+	}
+	
+	@GetMapping("/findAllUser")
+	public List<User> findAllUser(){
+		return userRepo.findAll();
+	}
+	
 	
 	@GetMapping("/patients")
 	public ResponseEntity<?>  getAllPatients(){
