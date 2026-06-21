c7ec48593d8d3317c6a1cc5b7e376ec57091c3d6
sangramsekhar81 <sangramsekhar83@gmail.com>
2026-06-21 23:17:08 +0530

    add oauth2 service config

--- src/main/java/com/spring/security/securityConfig/WebSecurityConfig.java
+++ src/main/java/com/spring/security/securityConfig/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.security.securityConfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig {
 
 //	private final PasswordEncoder passwordEncoder ;
@@ -29,7 +31,12 @@ public class WebSecurityConfig {
 //				.requestMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				)
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+				.oauth2Login(oAuth2->oAuth2.failureHandler(
+						(request, response, exception)->{
+							log.error("oAuth2 error:{}", exception.getMessage());
+						}
+				));
 //				.formLogin(form -> form
 //						.permitAll());
 				return httpSecurity.build();
